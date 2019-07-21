49218f08124557244188369c74ce8ee6e2a051f5
Nabil Tayeh <nabil_9000@hotmail.com>
2019-07-21 19:42:37 +0300

    Create Layout Pages

---
+++ resources/views/front/layouts/navbar.blade.php
@@ -0,0 +1,26 @@
+<div class="navbar">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a href="{{route('HomePage')}}" class="brand">
+                        <img src="{{ asset('images/logo.png') }}" width="120" height="40" alt="Logo" />
+                        <!-- This is website logo -->
+                    </a>
+                    <!-- Navigation button, visible on small resolution -->
+                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <i class="icon-menu"></i>
+                    </button>
+                    <!-- Main navigation -->
+                    <div class="nav-collapse collapse pull-right">
+                        <ul class="nav" id="top-navigation">
+                            <li class="active"><a href="#home">الرئيسية</a></li>
+                            <li><a href="#about">عن الشركة</a></li>
+                            <li><a href="#service">خدماتنا</a></li>
+                            
+                            <li><a href="#orders">اطلب الخدمة</a></li>
+                            <li><a href="#contact">إتصل بنا</a></li>
+                        </ul>
+                    </div>
+                    <!-- End main navigation -->
+                </div>
+            </div>
+        </div>
